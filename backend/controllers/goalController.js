9abf3dc09be17ad4bf7d44fb398ae44a17d332ed
adamklyne <adam.klyne@gmail.com>
2022-06-22 16:07:34 +0800

    Added JWT and user protection

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,13 @@
 const asyncHandler = require('express-async-handler')
+
 const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
 
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler(async(req, res) => {
-    const goals = await Goal.find()
+    const goals = await Goal.find({ user: req.user.id })
     res.status(200).json(goals)
 })
 
@@ -19,7 +21,8 @@ const createGoal = asyncHandler(async(req,res) => {
     }
 
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id,
     })
     res.status(200).json(goal)
 })
@@ -33,6 +36,20 @@ const updateGoal = asyncHandler(async(req, res) => {
     if(!goal){
         res.status(400)
         throw new Error('Goal not found')
+    }
+
+    const user = await User.findById(req.user.id)
+
+    // Check for user
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    // Ensure logged in user matches goal user
+    if(goal.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('User not authorised')
     }
 
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
@@ -49,7 +66,20 @@ const deleteGoal = asyncHandler(async(req,res) => {
         res.status(400)
         throw new Error('Goal not found')
     }
+    const user = await User.findById(req.user.id)
 
+    // Check for user
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    // Ensure logged in user matches goal user
+    if(goal.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('User not authorised')
+    }
+    
     const deleteGoal = await Goal.findByIdAndDelete(req.params.id)
     res.status(200).json(deleteGoal)
 })
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
+const path = require('path');
 const express = require('express');
-const colors = require('colors')
-const router = require('./routes/goalRoutes');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
-const connectDB =  require('./config/db')
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -14,7 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`))
